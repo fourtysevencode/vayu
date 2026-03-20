db97a04798022c069dc97c805d8e1452d2229079
Ronak <ronakpalforyou@gmail.com>
2026-03-21 01:43:44 +0530

    updated aqi dashboard

--- streamlit-app/pages/aqi_dashboard.py
+++ streamlit-app/pages/aqi_dashboard.py
@@ -6,19 +6,29 @@ import streamlit as st
 import pandas as pd
 from utils.aqi import get_multiple_cities, aqi_color, get_aqi
 
-st.set_page_config(page_title="AQI Dashboard", page_icon="🌫️")
-st.title("AQI Dashboard")
+st.set_page_config(page_title="Vayu AQI Dashboard", page_icon="🔰")
+st.title("Vayu AQI Dashboard")
 st.caption("Real time AQI Dashboard for selected cities")
 
-cities = st.multiselect(label = "city", options = ["Bangalore", "Mumbai", "Pune", "Jakarta", "Washington"])
+cities = st.multiselect(label = "Select one or more cities", options = ["Bangalore", "Mumbai", "Pune", "Jakarta", "Washington"])
 
 if st.button("🔃 Fetch Live Data"):
     if cities:
         with st.spinner("Fetching.."):
             data = get_multiple_cities(cities)
+
+            if not data: st.error("Check your API token")
+
+            df = pd.DataFrame(data)
+            df = df.replace("N/A", None)
+            
             for i in data:
                 st.header(i["city"])
                 st.write(i)
+
+            st.header("Dataframe with AQI Metrics of theSelected Cities")
+            st.write(df.head())
+        
     else:
         st.toast("Select one or more city!")
 
